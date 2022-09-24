3a612e6c063fbc8f5641054049892475f8634e52
Shane Peelar <lookatyouhacker@gmail.com>
2022-09-24 16:32:24 -0400

    Benchmarks for showing changeticks perf

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,13 +10,12 @@ fn bench(c: &mut Criterion) {
 
     //TODO: does it make sense to let Criterion specify the world size??
 
-    for exp in 14..21 {
+    for exp in 14..18 {
         let size = 2_i32.pow(exp);
 
-        //Each iteration processes (effectively) 3 f32s, one iteration processes size * 3 * f32 bytes
         group.throughput(criterion::Throughput::Elements(size as u64));
 
-        group.bench_with_input(BenchmarkId::new("naive", size), &size, |b, &size| {
+        /*group.bench_with_input(BenchmarkId::new("naive", size), &size, |b, &size| {
             let mut bench = impls::naive::Benchmark::new(size);
             b.iter(move || bench.run(rand::thread_rng().gen_range(0.0..=1.0)));
 
@@ -26,36 +25,53 @@ fn bench(c: &mut Criterion) {
                 BatchSize::SmallInput,
             )*/
         });
+        */
 
-        group.bench_with_input(
+        /*group.bench_with_input(
             BenchmarkId::new("naive_aligned", size),
             &size,
             |b, &size| {
                 let mut bench = impls::naive_aligned::Benchmark::new(size);
                 b.iter(move || bench.run(rand::thread_rng().gen_range(0.0..=1.0)));
             },
+        );*/
+
+        group.bench_with_input(
+            BenchmarkId::new("simd_batch_4_soa_native_nochangedetect", size),
+            &size,
+            |b, &size| {
+                let mut bench = impls::simd_batch_4::Benchmark::new(size);
+                b.iter(move || bench.run_nochange(rand::thread_rng().gen_range(0.0..=1.0)));
+            },
         );
 
-        //Each iteration processes 4 "virtual entities" by mapping one virtual entity to each SIMD lane.
-        //Therefore the total number of entities are the same
-        group.throughput(criterion::Throughput::Elements(size as u64));
+        group.bench_with_input(
+            BenchmarkId::new("simd_batch_4_soa_native", size),
+            &size,
+            |b, &size| {
+                let mut bench = impls::simd_batch_4::Benchmark::new(size);
+                b.iter(move || bench.run_change(rand::thread_rng().gen_range(0.0..=1.0)));
+            },
+        );
 
+        /*
         group.bench_with_input(BenchmarkId::new("aosoa_sse4", size), &size, |b, &size| {
             let mut bench = impls::aosoa_sse4::Benchmark::new(size); // TODO: rename f32x4
             b.iter(move || bench.run(rand::thread_rng().gen_range(0.0..=1.0)));
         });
+        */
 
-        group.bench_with_input(
+        /*group.bench_with_input(
             BenchmarkId::new("naive_batched_swizzle", size),
             &size,
             |b, &size| {
                 let mut bench = impls::naive_batched_swizzle::Benchmark::new(size); // TODO: rename f32x4
                 b.iter(move || bench.run(rand::thread_rng().gen_range(0.0..=1.0)));
             },
-        );
+        );*/
 
         group.bench_with_input(
-            BenchmarkId::new("simd_batch_sse4_soa_optimal", size),
+            BenchmarkId::new("simd_batch_4_soa_simulated", size),
             &size,
             |b, &size| {
                 let mut bench = impls::simd_batch_sse4::Benchmark::new(size);
@@ -63,6 +79,7 @@ fn bench(c: &mut Criterion) {
             },
         );
 
+        /*
         group.bench_with_input(
             BenchmarkId::new("simd_batch_sse4_soa_suboptimal", size),
             &size,
@@ -71,6 +88,7 @@ fn bench(c: &mut Criterion) {
                 b.iter(move || bench.run_suboptimal(rand::thread_rng().gen_range(0.0..=1.0)));
             },
         );
+        */
     }
 
     //TODO: ensure LTO, O3, -ffast-math, etc...
--- benches/impls/mod.rs
+++ benches/impls/mod.rs
@@ -2,4 +2,5 @@ pub mod aosoa_sse4;
 pub mod naive;
 pub mod naive_aligned;
 pub mod naive_batched_swizzle;
+pub mod simd_batch_4;
 pub mod simd_batch_sse4;
